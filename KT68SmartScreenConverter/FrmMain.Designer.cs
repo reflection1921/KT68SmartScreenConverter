d80523cc2dd477a236047fdc96289afd4d52f912
Reflection <firesoft1212@gmail.com>
2023-06-24 00:31:28 +0900

    초기 커밋.

---
+++ KT68SmartScreenConverter/FrmMain.Designer.cs
@@ -0,0 +1,75 @@
+﻿namespace KT68SmartScreenConverter
+{
+    partial class FrmMain
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(776, 67);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Cyberboard R1 JSON 파일";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Location = new System.Drawing.Point(12, 85);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(776, 100);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "KT68 JSON 파일";
+            // 
+            // FrmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "FrmMain";
+            this.Text = "KT68 Smartscreen Converter";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+    }
+}
+
---
+++ KT68SmartScreenConverter/KT68Converter.cs
@@ -0,0 +1,124 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms.Design;
+using System.Xml.Serialization;
+
+namespace KT68SmartScreenConverter
+{
+    internal class KT68Converter
+    {
+        public enum AlignType { Left, Center, Right };
+        JObject cbJObj;
+        JObject kt68JObj;
+        public KT68Converter(string cyberJsonData)
+        {
+            cbJObj = JObject.Parse(cyberJsonData);
+            GenerateKT68Json();
+        }
+
+        public string Convert(AlignType align, Color backgroundColor)
+        {
+            string color = "#" + backgroundColor.R.ToString("X2") + backgroundColor.G.ToString("X2") + backgroundColor.B.ToString("X2");
+            kt68JObj["led effects"][0]["frames"] = GetFrames(color, align);
+            File.WriteAllText("sss.json", kt68JObj.ToString());
+
+            return kt68JObj.ToString();
+
+        }
+
+        private void GenerateKT68Json()
+        {
+            kt68JObj = new JObject();
+            kt68JObj["MACHENIKE"] = "KT 68 LED";
+            
+
+            JObject effectItem = new JObject();
+            effectItem["brightness"] = 255;
+            effectItem["speed"] = 90;
+            effectItem["frames"] = new JArray();
+
+            JArray effects = new JArray
+            {
+                effectItem
+            };
+
+            kt68JObj["led effects"] = effects;
+
+
+        }
+
+        private JArray GetFrames(string color, AlignType align)
+        {
+            JObject effectItem = (JObject)cbJObj["lightEffects"][4]; //led screen
+            JArray frames = (JArray)effectItem["frames"];
+            JArray newFrames = new JArray();
+
+            for (int i = 0; i < frames.Count; i++)
+            {
+                JArray frameRGB = (JArray)frames[i]["frameRGB"];
+
+                JObject frameObj = new JObject();
+                frameObj["frameIndex"] = i + 1;
+                frameObj["frameRGB"] = ConvertFrameData(frameRGB, color, align);
+                newFrames.Add(frameObj);
+            }
+
+            return newFrames;
+        }
+
+        private JArray ConvertFrameData(JArray frameRGB, string color, AlignType align)
+        {
+            string[,] newFrame = new string[65, 5];
+
+            JArray newFrameJArr = new JArray();
+
+            int alignIdx = 0;
+            switch (align)
+            {
+                case AlignType.Center:
+                    alignIdx = 13;
+                    break;
+                case AlignType.Left:
+                    alignIdx = 0;
+                    break;
+                case AlignType.Right:
+                    alignIdx = 25;
+                    break;
+            }
+
+            for (int i = 0; i < frameRGB.Count; i++)
+            {
+                double num = (double)i / 5;
+                int truncated = (int)Math.Truncate(num);
+
+                int mod = i % 5;
+
+                newFrame[truncated + alignIdx, mod] = (string)frameRGB[i];
+            }
+
+
+            for (int y = 0; y < 5; y++)
+            {
+                for (int x = 0; x < 65; x++)
+                {
+                    if (newFrame[x, y] == null || newFrame[x, y].Length == 0)
+                    {
+                        newFrameJArr.Add(color);
+                        continue;
+                    }
+                    newFrameJArr.Add(newFrame[x, y]);
+                }
+            }
+
+            return newFrameJArr;
+        }
+
+
+    }
+}
